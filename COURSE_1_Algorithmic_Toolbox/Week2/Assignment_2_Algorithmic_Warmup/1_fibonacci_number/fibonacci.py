9e79170d6d7cf7b91508bf9849e55f9c2b0c3e7f
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-08 18:49:11 -0500

    Fix solution
    The first solution exceeds the time limit (with recursive solution)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/1_fibonacci_number/fibonacci.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/1_fibonacci_number/fibonacci.py
@@ -1,5 +1,10 @@
 def fibonacci(n):
 	if n <= 1:
 		return n
-	return fibonacci(n-1) + fibonacci(n-2)
+	Fn_2 = 0
+	Fn_1 = 1
+	for _ in range(n-1):
+		Fn = Fn_2 + Fn_1
+		Fn_2, Fn_1 = Fn_1, Fn
+	return Fn
 print(fibonacci(int(input(''))))
