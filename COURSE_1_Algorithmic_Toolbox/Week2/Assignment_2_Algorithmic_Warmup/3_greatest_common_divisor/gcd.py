c7da67a4101eaee563e26bc6a6dc66024d2c14f1
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-09 03:07:10 -0500

    Update the file in order to use optimized solution

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/3_greatest_common_divisor/gcd.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/3_greatest_common_divisor/gcd.py
@@ -2,12 +2,11 @@ def huge_fibo_number(tupla):
 	n = int(tupla[0])
 	m = int(tupla[1])
 	if n <= 1:
-		return n%m
-	Fn_2 = 0
-	Fn_1 = 1
-	for _ in range(n-1):
-		Fn = Fn_2 + Fn_1
-		#Fn = Fn%10
-		Fn_2, Fn_1 = Fn_1, Fn
-	return Fn%m
+		return n
+	final_gcd = 1
+	for d in range(2, min(n, m) * 1):
+		if n%d == 0 and m%d == 0:
+			if final_gcd < d:
+				final_gcd = d
+	return final_gcd
 print(huge_fibo_number(input('').split(' ')))
