11be783d8e0d94570433c433c9f5f9f469369d06
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-09 03:54:49 -0500

    Change the solution
    Change the solution to a inspired to the solution proposal on the example1.py file iin order to fix the following statement.
    
    Failed case #10/40: time limit exceeded
    Input:
    100000000 100000000
    
    Your output:
    100000000
    
    stderr:
    
     (Time used: 5.51/5.00, memory used: 11350016/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/3_greatest_common_divisor/gcd.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/3_greatest_common_divisor/gcd.py
@@ -1,9 +1,9 @@
-def huge_fibo_number(n, m):
-	final_gcd = 1
-	for d in range(2, min(n, m) + 1):
-		if n%d == 0 and m%d == 0:
-			if final_gcd < d:
-				final_gcd = d
-	return final_gcd
+def gcd(n, m):
+	if m == 0:
+		return n, 0
+	while m != 0:
+		n, m = m, n%m
+	return n
+
 n, m = map(int, input('').split(' '))
 print(huge_fibo_number(n, m))
