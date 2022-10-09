5bcea83be11785a1be9b843a43d7fd0c51c9b358
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-09 03:25:26 -0500

    Delete some lines and add map statement

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/3_greatest_common_divisor/gcd.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/3_greatest_common_divisor/gcd.py
@@ -1,12 +1,9 @@
-def huge_fibo_number(tupla):
-	n = int(tupla[0])
-	m = int(tupla[1])
-	if n <= 1:
-		return n
+def huge_fibo_number(n, m):
 	final_gcd = 1
 	for d in range(2, min(n, m) + 1):
 		if n%d == 0 and m%d == 0:
 			if final_gcd < d:
 				final_gcd = d
 	return final_gcd
-print(huge_fibo_number(input('').split(' ')))
+n, m - map(int(), input('').split(' '))
+print(huge_fibo_number())
