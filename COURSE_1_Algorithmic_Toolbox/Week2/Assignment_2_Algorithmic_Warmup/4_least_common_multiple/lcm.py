04e42b8e9f00b1dda54d83c938799538087dc40d
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-13 20:31:10 -0500

    Fix error
    Failed case #1/62: (Wrong answer)
    
    Input:
    18 35
    
    Your output:
    18
    
    Correct output:
    630
     (Time used: 0.01/5.00, memory used: 11227136/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/4_least_common_multiple/lcm.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/4_least_common_multiple/lcm.py
@@ -1,10 +1,10 @@
 def euclideanGCM(a, b):
 	if a == 0 or b == 0:
 		return max(a, b)
-	a = max(a, b)
-	b = min(a, b)
-	c = a%b
-	return euclideanGCM(b, c)
+	a2 = max(a, b)
+	b2 = min(a, b)
+	c = a2%b2
+	return euclideanGCM(b2, c)
 
 a, b = map(int, input().split())
 print(a*b//euclideanGCM(a, b))
