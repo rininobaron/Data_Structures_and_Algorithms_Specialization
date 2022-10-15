2130b99c2957ae7212e3eec5eb29a820c7fff26b
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-15 00:25:32 -0500

    Limit range in for loop by n mod m

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,12 +1,13 @@
 def huge_fibo(n, m):
 	if n <= 1:
-		return n%m
+		return n
 	Fn_2 = 0
 	Fn_1 = 1
-	for _ in range(n-1):
-		Fn = (Fn_2 + Fn_1)%m
-		Fn_2, Fn_1 = Fn_1, Fn
+	for _ in range(n%m-1):
+		Fn = Fn_2%m + Fn_1%m
+		Fn_2, Fn_1 = Fn_1%m, Fn%m
 	return Fn
 	
 n, m = map(int, input('').split(' '))
+#n = int(input(''))
 print(huge_fibo(n, m))
