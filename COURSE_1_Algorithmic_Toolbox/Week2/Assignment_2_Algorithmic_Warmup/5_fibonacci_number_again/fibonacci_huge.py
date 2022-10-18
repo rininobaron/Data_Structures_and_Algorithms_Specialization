c7fea092ce4692244c74c06a80f2d7aacd2a480a
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-18 04:58:58 -0500

    Fix error
    Failed case #7/24: (Wrong answer)
    
    Input:
    9999999999999 2
    
    Your output:
    2
    
    Correct output:
    0
     (Time used: 0.01/5.00, memory used: 11456512/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -3,9 +3,14 @@ def huge_fibo(n, m):
 		return n
 	Fn_2 = 0
 	Fn_1 = 1
-	for _ in range(n%10000-1):
+	end = n%m
+	#print(end)
+	if end <= 1:
+		return 0
+	#end = 0
+	for _ in range(end-1):
 		Fn = Fn_2%m + Fn_1%m
-		Fn_2, Fn_1 = Fn_1%m, Fn%m
+		Fn_2, Fn_1 = Fn_1%m, Fn
 	return Fn
 	
 n, m = map(int, input('').split(' '))
