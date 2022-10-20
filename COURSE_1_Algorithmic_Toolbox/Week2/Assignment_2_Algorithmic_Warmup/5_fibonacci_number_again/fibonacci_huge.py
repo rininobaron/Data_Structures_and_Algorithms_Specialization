3b3ddd81f04e15bc8cce6f385100db31daf50c79
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-20 04:30:32 -0500

    Add 'fibo', 'pisano' and modify 'huge_fibo'
    In order to solve overcome the following error.
    
    Failed case #7/24: (Wrong answer)
    
    Input:
    9999999999999 2
    
    Your output:
    2
    
    Correct output:
    0
     (Time used: 0.01/5.00, memory used: 11456512/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,17 +1,25 @@
-def huge_fibo(n, m):
+def fibo(n):
 	if n <= 1:
 		return n
 	Fn_2 = 0
 	Fn_1 = 1
-	end = n%m
-	#print(end)
-	if end <= 1:
-		return 0
-	#end = 0
-	for _ in range(end-1):
-		Fn = Fn_2%m + Fn_1%m
-		Fn_2, Fn_1 = Fn_1%m, Fn
+	for _ in range(n-1):
+		Fn = Fn_2 + Fn_1
+		Fn_2, Fn_1 = Fn_1, Fn
 	return Fn
+
+def pisano(m):
+	Fn_2 = 0
+	Fn_1 = 1
+	for i in range(m**2):
+		Fn = (Fn_2 + Fn_1)%m
+		Fn_2, Fn_1 = Fn_1, Fn
+		# A Pisano Period starts with 01
+		if Fn_2 == 0 and Fn_1 == 1:
+			return i + 1
+
+def huge_fibo(n, m):
+	return fibo(n%pisano(m))%m
 	
 n, m = map(int, input('').split(' '))
 #n = int(input(''))
