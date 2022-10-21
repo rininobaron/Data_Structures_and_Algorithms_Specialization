2407f544239cdba4adf6ac424f8e6faee80dc6d2
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-21 00:05:08 -0500

    Fix error
    Failed case #8/16: time limit exceeded
    Input:
    9999
    
    Your output:
    0
    
    stderr:
    
     (Time used: 6.67/5.00, memory used: 11341824/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,17 +1,16 @@
-def fibo(n):
+def last_digit_of_the_sum_of_fibos(n):
 	if n <= 1:
 		return n
 	Fn_2 = 0
 	Fn_1 = 1
-	for _ in range(n-1):
-		Fn = Fn_2 + Fn_1
-		Fn_2, Fn_1 = Fn_1, Fn
-	return Fn
-
-def last_digit_of_the_sum_of_fibos(n):
 	fibo_sum = 0
 	for i in range(n+1):
-		fibo_sum += fibo(i)
+		if i == 0 or i == 1:
+			fibo_sum += i
+			continue
+		Fn = Fn_2 + Fn_1
+		Fn_2, Fn_1 = Fn_1, Fn
+		fibo_sum += Fn
 	return fibo_sum%10
 
 print(last_digit_of_the_sum_of_fibos(int(input(''))))
