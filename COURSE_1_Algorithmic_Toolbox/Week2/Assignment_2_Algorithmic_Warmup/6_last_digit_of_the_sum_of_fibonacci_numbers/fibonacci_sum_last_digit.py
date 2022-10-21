b639ccd69d1a300cbd44d31ea0208a3e431f4cea
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-21 03:27:39 -0500

    Fix error (FINAL)
    Failed case #13/16: time limit exceeded
    Input:
    832564823476
    
    Your output:
    
    stderr:
    
     (Time used: 9.99/5.00, memory used: 11218944/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,13 +1,25 @@
+def pisano(m):
+	Fn_2 = 0
+	Fn_1 = 1
+	for i in range(m**2):
+		Fn = (Fn_2 + Fn_1)%m
+		Fn_2, Fn_1 = Fn_1, Fn
+		# A Pisano Period starts with 01
+		if Fn_2 == 0 and Fn_1 == 1:
+			return i + 1
+
 def last_digit_of_the_sum_of_fibos(n):
+	n = n%pisano(10)
 	if n <= 1:
 		return n
 	Fn_2 = 0
 	Fn_1 = 1
 	fibo_sum = 1
-	for _ in range(2, n+1):
+	for i in range(2, n+1):
 		Fn = (Fn_2 + Fn_1)%10
 		Fn_2, Fn_1 = Fn_1, Fn
 		fibo_sum += Fn
 	return fibo_sum%10
 
 print(last_digit_of_the_sum_of_fibos(int(input(''))))
+#print(pisano(int(input(''))))
