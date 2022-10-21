16dcac833c83ba23959c5a897d0351b5d6735655
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-21 04:39:36 -0500

    Fix error (FINAL)
    Add 'pisano' function and
    
    Failed case #16/22: time limit exceeded
    Input:
    613455
    
    Your output:
    0
    
    stderr:
    
     (Time used: 8.59/5.00, memory used: 11345920/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ COURSE_1_Algorithmic_Toolbox/Week2/Assignment_2_Algorithmic_Warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -8,7 +8,18 @@ def fibo(n):
 		Fn_2, Fn_1 = Fn_1, Fn
 	return Fn
 
+def pisano(m):
+	Fn_2 = 0
+	Fn_1 = 1
+	for i in range(m**2):
+		Fn = (Fn_2 + Fn_1)%m
+		Fn_2, Fn_1 = Fn_1, Fn
+		# A Pisano Period starts with 01
+		if Fn_2 == 0 and Fn_1 == 1:
+			return i + 1
+
 def sum_fibo_squares(n):
+	n = n%pisano(10)
 	fibo_n = fibo(n)
 	fibo_sum = fibo_n*(fibo_n + fibo(n-1))
 	return fibo_sum%10
