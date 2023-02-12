8ef993b16e3708a6e4cf526a33162639dcda260f
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-02-12 01:30:34 -0600

    Add first solution proposal for 1_money_change

---
+++ COURSE_1_Algorithmic_Toolbox/Week3/1_money_change/change.py
@@ -0,0 +1,13 @@
+'''
+This function calculate the minimum number of coins with values 1,2,5
+needed to change a given auanity of money
+'''
+def change(money):
+	if money == 0:
+		return 0
+	denom = [1,2,5]
+	maxCoin = max(denom)
+	while maxCoin > money:
+		denom.remove(maxCoin)
+		maxCoin = max(denom)
+	return 1 + change(maxCoin)
