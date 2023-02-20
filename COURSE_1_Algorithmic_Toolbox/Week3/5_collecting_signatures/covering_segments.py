7d791f3b687af8506f8ef84f8972cddb820e58a8
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-02-20 02:55:08 -0600

    Add third solution proposal tested locally
    In order to solve the error below>
    
    Failed case #4/15: Wrong answer
    got: 27 expected: 26

--- COURSE_1_Algorithmic_Toolbox/Week3/5_collecting_signatures/covering_segments.py
+++ COURSE_1_Algorithmic_Toolbox/Week3/5_collecting_signatures/covering_segments.py
@@ -34,52 +34,20 @@ def get_points():
 			counter+=1
 		pairs_sort.append(pair_min_r)
 		pairs.pop(target_index)
-	# Appen index to each pair in pairs_sort
-	indexes = list(range(len(pairs_sort)))
-	for index,_ in enumerate(pairs_sort):
-		pairs_sort[index].append(index)
-	# Look for minimum points
-	start = pairs_sort[0][0]
-	final_points = []
 	#print(pairs_sort)
-	while len(pairs_sort) > 0:
-		temp_dict = {}
+	# Get points
+	final_points = []
+	while len(pairs_sort)>0:
+		temp_agg = []
+		pair_ref = pairs_sort[0]
 		for pair in pairs_sort:
-			if start > pair[1]:
-				start = pair[0]
-			#print("start: "+str(start))
-			for point in range(start,pair[1]+1):
-				for pair_j in pairs_sort:
-					if pair_j[0] <= point and pair_j[1] >= point:
-						if point not in temp_dict.keys():
-							temp_dict[point] = []
-						temp_dict[point].append(pair_j[2])
-		# Look for the point with more segments "touched"
-		# If there are many points, the maximum is choice
-		target_len = 0
-		final_point = 0
-		for point in temp_dict.keys():
-			if target_len < len(temp_dict[point]):
-				final_point = point
-				target_len = len(temp_dict[point])
-			elif target_len == len(temp_dict[point]):
-				if final_point < point:
-					final_point=point
-					target_len = len(temp_dict[point])
-		# Remove the pairs (segments) "touched" by the point
-		for index in temp_dict[final_point]:
-			for pair in pairs_sort:
-				if pair[2] == index:
-					pairs_sort.remove(pair)
-		start = max(temp_dict.keys())+1
+			if pair_ref[1] >= pair[0] and pair_ref[1] <= pair[1]:
+				temp_agg.append(pair)
+		final_point = temp_agg[0][1]
+		for pair in temp_agg:
+			pairs_sort.remove(pair)
 		final_points.append(final_point)
-		#print(temp_dict)
-	final_points.sort()
-	final_points = list(set(final_points))
-	#print()
-	#print(final_points)
-	return final_points 
-
+	return final_points
 
 final_points = get_points()
 len_ = len(final_points)
