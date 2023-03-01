3c38f975bcc86f4c9b8dd27d58ffc98c04974f46
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-01 02:08:09 -0600

    Add solution test locally

--- COURSE_1_Algorithmic_Toolbox/Week3/6_maximum_number_of_prizes/different_summands.py
+++ COURSE_1_Algorithmic_Toolbox/Week3/6_maximum_number_of_prizes/different_summands.py
@@ -2,32 +2,26 @@ def max_number():
 	n = int(input(""))
 	suma = 0
 	numbers = []
-	if n%2 == 0:
-		number = n/2
+	if n==1:
+		return 1, str(1)
+	if n==2:
+		return 1, str(2)
+	number = 1
+	while suma < n:
+		suma+=number
 		numbers.append(number)
-		suma=number
-		while suma != n:
-			number-=1
-			suma+=number
-			if suma > n:
-				suma-=number
-				continue
-			numbers.append(number)
-	else:
-		number = n/2+1
-		numbers.append(number)
-		suma=number
-		while suma != n:
-			number-=1
-			suma+=number
-			if suma > n:
-				suma-=number
-				continue
-			numbers.append(number)
+		# Avoid overcome or repeat number 
+		if n-suma<=number:
+			suma-=number
+			numbers.remove(number)
+			numbers.append(n-suma)
+			break
+		number+=1
 	k = len(numbers)
+	numbers.sort()
 	numbers = [str(int(i)) for i in numbers]
 	numbers=" ".join(numbers)
-	return k,numbers
+	return k, numbers
 
 k,numbers = max_number()
 print(k)
