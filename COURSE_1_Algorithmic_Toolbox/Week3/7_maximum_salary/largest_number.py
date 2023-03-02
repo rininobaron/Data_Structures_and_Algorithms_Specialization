80360c00c8364232043da8f759e8cce758e5550e
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-02 03:07:27 -0600

    Rebuild solution in order to fix 'Failed case #6/11: Wrong answer'

--- COURSE_1_Algorithmic_Toolbox/Week3/7_maximum_salary/largest_number.py
+++ COURSE_1_Algorithmic_Toolbox/Week3/7_maximum_salary/largest_number.py
@@ -6,20 +6,33 @@ def largest_concatenate():
 		return print("ERROR\nThe numbers are not "+str(n))
 	largest_number = ""
 	while len(numbers) > 0:
-		fullNumber = numbers[0]
-		maxNumber = numbers[0][0]
+		bestNumber=numbers[0]
 		for i,number in enumerate(numbers):
 			if i==0:
 				continue
-			if int(number[0]) > int(maxNumber):
-				fullNumber=number
-				maxNumber=number[0]
-			elif int(number[0]) == int(maxNumber):
-				if int(number[-1]) > int(fullNumber[-1]):
-					fullNumber=number
-					maxNumber=number[0]
-		largest_number+=fullNumber
-		numbers.remove(fullNumber)
+			if len(bestNumber) > len(number):
+				major=bestNumber
+				minor=number
+			elif len(bestNumber) < len(number):
+				major=number
+				minor=bestNumber
+			else:
+				if bestNumber==number:
+					continue
+				else:
+					major=bestNumber
+					minor=number
+			if int(major[:len(minor)]) > int(minor):
+				bestNumber=major
+			elif int(major[:len(minor)]) < int(minor):
+				bestNumber=minor
+			else:
+				if int(major[len(minor):]) >= int(minor[:len(major[len(minor):])]):
+					bestNumber=major
+				else:
+					bestNumber=minor
+		largest_number+=bestNumber
+		numbers.remove(bestNumber)
 	return largest_number
 
 print(largest_concatenate())
