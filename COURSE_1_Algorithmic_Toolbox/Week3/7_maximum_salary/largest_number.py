7c51ec324016d9bf5a39782fd7d08b738e6f6959
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-03 01:14:47 -0600

    Rebuild solution, tested locally

--- COURSE_1_Algorithmic_Toolbox/Week3/7_maximum_salary/largest_number.py
+++ COURSE_1_Algorithmic_Toolbox/Week3/7_maximum_salary/largest_number.py
@@ -8,35 +8,17 @@ def largest_concatenate():
 	while len(numbers) > 0:
 		bestNumber=numbers[0]
 		for i,number in enumerate(numbers):
-			if i==0:
-				continue
-			if len(bestNumber) > len(number):
-				major=bestNumber
-				minor=number
-			elif len(bestNumber) < len(number):
-				major=number
-				minor=bestNumber
-			else:
-				if bestNumber==number:
-					continue
-				else:
-					major=bestNumber
-					minor=number
-			if int(major[:len(minor)]) > int(minor):
-				bestNumber=major
-			elif int(major[:len(minor)]) < int(minor):
-				bestNumber=minor
-			else:
-				temp=major[len(minor):]
-				temp=[int(digit) for digit in temp]
-				major_max=max(temp)
-				minor_max=max([int(digit) for digit in minor])
-				if major_max > minor_max:
-					bestNumber=major
-				else:
-					bestNumber=minor
+			bestNumber=isBetter(number,bestNumber)
 		largest_number+=bestNumber
 		numbers.remove(bestNumber)
 	return largest_number
+
+def isBetter(number1,number2):
+	num1 = number1+number2
+	num2 = number2+number1
+	if str(max(int(num1),int(num2))) == num1:
+		return number1
+	else:
+		return number2
 
 print(largest_concatenate())
