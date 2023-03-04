8b63566ad87dd94f94974184e1032f8b4b7229b1
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-04 00:50:06 -0600

    Add first challenge script from Khan Academy

---
+++ COURSE_1_Algorithmic_Toolbox/Week4/PRACTICE/challenge1.js
@@ -0,0 +1,34 @@
+/*
+This code challenge is from Khan Academy
+https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/pc/challenge-binary-search
+*/
+
+/* Returns either the index of the location in the array,
+  or -1 if the array did not contain the targetValue */
+var doSearch = function(array, targetValue) {
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+    while(max >= min){
+        guess = Math.floor((max+min)/2);
+        if(targetValue===array[guess]){
+            return guess; 
+        }
+        else if(array[guess]<targetValue){
+            min = guess + 1;
+        }
+        else{
+            max = guess - 1;  
+        }
+    }
+    return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+        41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+//println("Found prime at index " + result); //Only in web browsre
+console.log("Found prime at index " + result); // Linux Terminal
+
+//Program.assertEqual(doSearch(primes, 73), 20);
