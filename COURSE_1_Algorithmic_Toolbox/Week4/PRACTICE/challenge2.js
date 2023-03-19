b7415f77b424725443efabd7140daf318c43dcb1
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-19 00:44:39 -0600

    Update step 2

--- COURSE_1_Algorithmic_Toolbox/Week4/PRACTICE/challenge2.js
+++ COURSE_1_Algorithmic_Toolbox/Week4/PRACTICE/challenge2.js
@@ -10,3 +10,7 @@ swap(testArray, 0, 1);
 console.log(testArray);
 
 //Program.assertEqual(testArray, [9, 7, 4]);
+//swap(testArray, 0, 2);
+//Program.assertEqual(testArray, [4, 7, 9]);
+//swap(testArray, 1, 2);
+//Program.assertEqual(testArray, [4, 9, 7]);
