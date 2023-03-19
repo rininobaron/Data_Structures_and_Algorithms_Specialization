d860f4c499532a3d8806a8176af9105bfacc9d80
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-18 21:47:39 -0600

    Add second challenge in JavaScript

---
+++ COURSE_1_Algorithmic_Toolbox/Week4/PRACTICE/challenge2.js
@@ -0,0 +1,12 @@
+var swap = function(array, firstIndex, secondIndex) {
+	var temp = array[firstIndex];
+	array[firstIndex] = array[secondIndex];
+	array[secondIndex] = temp;
+};
+
+var testArray = [7, 9, 4];
+swap(testArray, 0, 1);
+
+console.log(testArray);
+
+//Program.assertEqual(testArray, [9, 7, 4]);
