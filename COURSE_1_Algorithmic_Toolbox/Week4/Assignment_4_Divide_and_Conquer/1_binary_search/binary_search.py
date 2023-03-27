224f2ab3c2ad5eaccba3134dbff1ca853ee9cf8f
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-27 14:22:56 -0600

    First attempt

---
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/1_binary_search/binary_search.py
@@ -0,0 +1,36 @@
+def binary_search(array, number):
+	minIndex = 0
+	maxIndex = len(array)
+	index = (maxIndex+minIndex)//2
+	while maxIndex>=minIndex:
+		if array[index]==number:
+			return index
+		elif array[index]>number:
+			minIndex = index + 1
+			index = (maxIndex+minIndex)//2
+		elif array[index]<number:
+			maxIndex = index - 1
+			index = (maxIndex+minIndex)//2
+	return -1
+
+def multi_search():
+	n = int(input(""))
+	array = input("") # array K
+	array = array.split(" ")
+	array = [int(i) for i in array]
+	if len(array) != n:
+		print("ERROR!")
+		print("The length of the array K is not "+str(n))
+	m = int(input(""))
+	array2 = input("") # array Q
+	array2 = array2.split(" ")
+	array2 = [int(i) for i in array2]
+	if len(array2) != m:
+		print("ERROR!")
+		print("The length of the array Q is not "+str(n))
+	final_array = []
+	for number in array2:
+		final_array.append(binary_search(array, number))
+	return " ".join(final_array)
+
+multi_search()
