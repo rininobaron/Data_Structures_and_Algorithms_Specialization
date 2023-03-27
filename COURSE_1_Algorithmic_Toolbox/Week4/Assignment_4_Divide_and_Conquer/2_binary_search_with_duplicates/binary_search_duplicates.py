d9c2f54ce78d91a8092f25da27ef5b66996fc9fd
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-27 16:10:39 -0600

    First attempt

---
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/2_binary_search_with_duplicates/binary_search_duplicates.py
@@ -0,0 +1,42 @@
+def binary_search_dupli(array, number):
+	minIndex = 0
+	maxIndex = len(array)-1
+	index = (maxIndex+minIndex)//2
+	while maxIndex>=minIndex:
+		if array[index]==number:
+			while index>0:
+				index = index-1
+				if array[index]==number:
+					continue
+				else:
+					return index + 1
+			return index
+		elif array[index]>number:
+			maxIndex = index - 1
+			index = (maxIndex+minIndex)//2
+		elif array[index]<number:
+			minIndex = index + 1
+			index = (maxIndex+minIndex)//2
+	return -1
+
+def multi_search():
+	n = int(input(""))
+	array = input("") # array K
+	array = array.split(" ")
+	array = [int(i) for i in array]
+	if len(array) != n:
+		print("ERROR!")
+		print("The length of the array K is not "+str(n))
+	m = int(input(""))
+	array2 = input("") # array Q
+	array2 = array2.split(" ")
+	array2 = [int(i) for i in array2]
+	if len(array2) != m:
+		print("ERROR!")
+		print("The length of the array Q is not "+str(n))
+	final_array = []
+	for number in array2:
+		final_array.append(binary_search_dupli(array, number))
+	return print(" ".join([str(i) for i in final_array]))
+
+multi_search()
