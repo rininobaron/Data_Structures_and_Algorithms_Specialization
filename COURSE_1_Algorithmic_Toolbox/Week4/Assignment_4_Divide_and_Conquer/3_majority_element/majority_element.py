79980d288ff90c9ed61ccf38fb566e22a31ba9c1
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-27 22:26:35 -0600

    Fix time issue
    Failed case #11/21: time limit exceeded (Time used: 10.00/5.00, memory used: 22966272/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/3_majority_element/majority_element.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/3_majority_element/majority_element.py
@@ -6,21 +6,19 @@ def majority_element():
 	if n != len(array):
 		return print("ERROR!\nThe length of array is not "+str(n))
 	n_2 = n/2
-	counter=0
-	pivots=[]
-	while len(array)>counter:
-		pivot = array[counter]
-		if pivot in pivots:
-			counter+=1
-			continue
-		pivots.append(pivot)
+	while len(array)>0:
+		pivot = array[0]
 		suma=0
-		for number in array:
+		counter=0
+		len_ref=len(array)
+		while len_ref>counter:
+			number=array[0]
 			if number==pivot:
 				suma+=1
 				if suma>n_2:
 					return print(1)
-		counter+=1
+			array.remove(number)
+			counter+=1
 	return print(0)
 
 majority_element()
