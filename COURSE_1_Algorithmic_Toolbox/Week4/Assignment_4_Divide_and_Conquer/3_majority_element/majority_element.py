6d04ff95070b6ff59c6eb9a58661e177d62e09d2
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-28 13:21:57 -0600

    Change to counter_dict strategy, final solution

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/3_majority_element/majority_element.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/3_majority_element/majority_element.py
@@ -6,12 +6,12 @@ def majority_element():
 	if n != len(array):
 		return print("ERROR!\nThe length of array is not "+str(n))
 	n_2 = n/2
-	array.sort()
-	while len(array)>0:
-		index = array.index(array[-1])
-		count = len(array) - index
-		array = array[:index]
-		if count > n_2:
+	counter_dict={}
+	for number in array:
+		if number not in counter_dict.keys():
+			counter_dict[number]=0
+		counter_dict[number]+=1
+		if counter_dict[number] > n_2:
 			return print(1)
 	return print(0)
 
