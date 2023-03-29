49777c541d2fe92cd4f77d5a60b8e6f755b36461
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-28 18:47:29 -0600

    Use counter dict startegy in order to fix time issue
    Failed case #4/23: time limit exceeded (Time used: 10.00/5.00, memory used: 21000192/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/4_improving_quicksort/sorting.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/4_improving_quicksort/sorting.py
@@ -1,39 +1,21 @@
-import random
-
-def swap(array, i, minIndex):
-	temp=array[i]
-	array[i]=array[minIndex]
-	array[minIndex]=temp
-	return array
-
-def selection_sort(array):
-	for i in range(len(array)):
-		minIndex=i
-		for j in range(i+1,len(array)):
-			if array[j] < array[minIndex]:
-				minIndex=j
-		array = swap(array, i, minIndex)
-	return array
-
-def radomized_quick_sort():
+def sort_custom():
 	n=int(input(""))
 	array=input("")
 	array=array.split(" ")
 	array=[int(i) for i in array]
 	if len(array)!=n:
 		return print("ERROR!\nThe length of array is not "+str(n))
-	m=random.choice(array)
-	array1=[]
-	array2=[]
-	for i in range(len(array)):
-		if array[i] <=m:
-			array1.append(array[i])
-		else:
-			array2.append(array[i])
-	array1=selection_sort(array1)
-	array2=selection_sort(array2)
-	array=array1+array2
+	counter_dict={}
+	for number in array:
+		if number not in counter_dict.keys():
+			counter_dict[number]=0
+		counter_dict[number]+=1
+	keys_sorted=list(counter_dict.keys())
+	keys_sorted.sort()
+	array=[]
+	for key in keys_sorted:
+		array+=[key]*counter_dict[key]
 	array=[str(i) for i in array]
 	return print(" ".join(array))
 
-radomized_quick_sort()
+sort_custom()
