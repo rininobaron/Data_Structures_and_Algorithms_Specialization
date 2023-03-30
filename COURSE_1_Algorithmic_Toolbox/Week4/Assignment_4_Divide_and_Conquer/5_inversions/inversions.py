7c3520df1ce997ea9c2bddadc40efb6a00168b15
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-29 23:09:24 -0600

    Use Naive solution in order to overcome failed case
    Failed case #3/14: (Wrong answer)
    
    Input:
    6
    9 8 7 3 2 1
    
    Your output:
    3
    
    Correct output:
    15
     (Time used: 0.02/5.00, memory used: 13434880/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/5_inversions/inversions.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/5_inversions/inversions.py
@@ -1,31 +1,33 @@
-import random
-
 def swap(array, i, minIndex):
 	temp=array[i]
 	array[i]=array[minIndex]
 	array[minIndex]=temp
 	return array
 
-def selection_sort(array):
+def count_swaps(array):
 	counter=0
-	for i in range(len(array)):
-		minIndex=i
-		for j in range(i+1,len(array)):
-			if array[j] < array[minIndex]:
-				minIndex=j
-		if i != minIndex:
-			array = swap(array, i, minIndex)
-			counter+=1
-	return array, counter
+	counterFlag=0
+	while counterFlag!=len(array):
+		counterFlag=0
+		for i in range(len(array)):
+			if i==len(array)-1:
+				counterFlag+=1
+				continue
+			if array[i] > array[i+1]:
+				array = swap(array, i, i+1)
+				counter+=1
+			else:
+				counterFlag+=1
+	return counter
 
-def radomized_quick_sort():
+def counter():
 	n=int(input(""))
 	array=input("")
 	array=array.split(" ")
 	array=[int(i) for i in array]
 	if len(array)!=n:
 		return print("ERROR!\nThe length of array is not "+str(n))
-	array,counter=selection_sort(array)
+	counter=count_swaps(array)
 	return print(counter)
 
-radomized_quick_sort()
+counter()
