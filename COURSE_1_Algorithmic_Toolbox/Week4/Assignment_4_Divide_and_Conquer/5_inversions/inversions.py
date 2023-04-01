3127f625db4bf330d2272fd32dd773a6527fda1b
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-31 18:40:56 -0600

    Use merge sort strategy
    Error fixed:
    
    Failed case #4/14: time limit exceeded (Time used: 8.44/5.00, memory used: 13381632/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/5_inversions/inversions.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/5_inversions/inversions.py
@@ -1,4 +1,21 @@
-def count_swaps(array):
+def merge(array1, array2):
+	counter=0
+	array=[]
+	while len(array1)>0 and len(array2)>0:
+		if array1[0]<=array2[0]:
+			array.append(array1[0])
+			array1.remove(array1[0])
+		else:
+			counter+=len(array1)
+			array.append(array2[0])
+			array2.remove(array2[0])
+	if len(array1)>0:
+		array=array+array1
+	elif len(array2)>0:
+		array=array+array2
+	return array,counter
+
+def sort_count_swaps(array):
 	array2=[]
 	counter=0
 	while len(array)>0:
@@ -13,7 +30,7 @@ def count_swaps(array):
 			array2.append(array[minIndex])
 			array.remove(array[minIndex])
 			counter+=minIndex
-	return counter
+	return array2,counter
 
 def counter():
 	n=int(input(""))
@@ -22,7 +39,12 @@ def counter():
 	array=[int(i) for i in array]
 	if len(array)!=n:
 		return print("ERROR!\nThe length of array is not "+str(n))
-	counter=count_swaps(array)
-	return print(counter)
+	m=len(array)//2
+	array1=array[:m].copy()
+	array2=array[m:].copy()
+	array1,counter1=sort_count_swaps(array1)
+	array2,counter2=sort_count_swaps(array2)
+	array,counter=merge(array1,array2)
+	return print(counter+counter1+counter2)
 
 counter()
