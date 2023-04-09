627fb51b442f556e4ead4d26018966b6d9dba1fe
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-09 00:18:12 -0600

    Delete map_ref
    In order to overcome memormy issue below:
    Failed case #4/25: time limit exceeded (Time used: 40.90/20.00, memory used: 1129025536/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/6_organizing_a_lottery/points_and_segments.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/6_organizing_a_lottery/points_and_segments.py
@@ -9,14 +9,12 @@ def counter():
     points_int=[int(i) for i in points]
     if len(points) != m:
         return print(f"ERROR!\nThe number of points are not {m}")
-    map_ref=[]
+    counter_dict={point:0 for point in points}
     for point in points_int:
         for l,r in zip(l_array,r_array):
-            map_ref.append([point,sorted([l,r,point])])
-    counter_dict={point:0 for point in points}
-    for array in map_ref:
-        if array[0]==array[1][1]:
-            counter_dict[str(array[0])]+=1
+            temp=sorted([l,r,point])
+            if point==temp[1]:
+                counter_dict[str(point)]+=1
     return print(" ".join([str(i) for i in counter_dict.values()]))
 
 counter()
