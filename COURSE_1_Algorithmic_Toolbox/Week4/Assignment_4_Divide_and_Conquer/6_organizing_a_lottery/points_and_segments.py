35589afad182b7e43ef1dbd70d2d690806f35552
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-09 03:53:33 -0600

    Fix solution, Final Solution

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/6_organizing_a_lottery/points_and_segments.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/6_organizing_a_lottery/points_and_segments.py
@@ -3,13 +3,13 @@ def counter():
     array=[]
     for _ in range(n):
         l, r = map(int, input().split(" "))
-        array.append([int(l),"l"])
-        array.append([int(r),"r"])
+        array.append((int(l),"l"))
+        array.append((int(r),"r"))
     points = input().split(" ")
     if len(points) != m:
         return print(f"ERROR!\nThe number of points are not {m}")
     for point in points:
-        array.append([int(point),"p"])
+        array.append((int(point),"p"))
     array.sort()
     segments=0
     counter_dict={}
@@ -19,7 +19,7 @@ def counter():
         elif item[1]=="r":
             segments-=1
         else:
-            counter_dict[str(item[0])]=counter_dict.get(str(item[0]),0)+segments
+            counter_dict[str(item[0])]=segments
     counter_list=[str(counter_dict[str(point)]) for point in points]
     return print(" ".join(counter_list))
 
