02be4e979f4e028837d2e7fe8d025232e97e4609
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-09 03:06:03 -0600

    Optimize the solution in order to overcome an unknown issue
    Maybe this issue is regarding memory:
    Failed case #4/25: unknown signal 9 (Time used: 12.20/20.00, memory used: 2115145728/2147483648.)
    
    But the solution outputs the following:
    Failed case #4/25: Wrong answer
     (Time used: 0.12/20.00, memory used: 26898432/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/6_organizing_a_lottery/points_and_segments.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/6_organizing_a_lottery/points_and_segments.py
@@ -9,12 +9,12 @@ def counter():
     points_int=[int(i) for i in points]
     if len(points) != m:
         return print(f"ERROR!\nThe number of points are not {m}")
+    map_ref=list(zip(l_array*m,r_array*m, points_int*n))
+    map_ref=list(map(lambda x: [sorted([x[0]]+[x[1]]+[x[2]]), x[2]], map_ref))
     counter_dict={point:0 for point in points}
-    for point in points_int:
-        for l,r in zip(l_array,r_array):
-            temp=sorted([l,r,point])
-            if point==temp[1]:
-                counter_dict[str(point)]+=1
+    for array in map_ref:
+        if array[0][1]==array[1]:
+            counter_dict[str(array[1])]+=1
     return print(" ".join([str(i) for i in counter_dict.values()]))
 
 counter()
