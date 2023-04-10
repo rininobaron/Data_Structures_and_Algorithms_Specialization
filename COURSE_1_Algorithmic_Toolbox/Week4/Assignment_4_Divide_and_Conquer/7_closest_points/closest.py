8a6b49524c8771451025ac1ed875863d80d68bbe
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-10 01:59:14 -0600

    Change initialization min_d by float('inf')

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/7_closest_points/closest.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/7_closest_points/closest.py
@@ -1,5 +1,5 @@
 def min_partial_distance(points,half_x):
-	min_d=10**9
+	min_d=float("inf")
 	distances=[]
 	counter=0
 	for point in points:
@@ -21,7 +21,7 @@ def strip_points(points,distances,d):
 	return points_final
 
 def min_final_distance(points):
-	min_d=10**9
+	min_d=float("inf")
 	counter=0
 	for point in points:
 		for index,point2 in enumerate(points):
@@ -42,22 +42,18 @@ def min_global_distance():
 		Y.append(y)
 	points=list(zip(X,Y))
 	points.sort()
-	if len(X)%2==0:
-		half_x=(points[int(len(X)/2)][0]+points[int(len(X)/2-1)][0])/2
-	else:
-		half_x=points[len(X)//2][0]
-	points1,points2=[],[]
-	for point in points:
-		if point[0]<half_x:
-			points1.append(point)
-		else:
-			points2.append(point)
+	points1=points[:len(X)//2]
+	points2=points[len(X)//2:]
+	#half_x=(points1[-1][0]+points2[0][0])/2
+	#half_x=(max(X)+min(X))/2
+	half_x=points[len(X)//2][0]
 	min_d1,distances1=min_partial_distance(points1,half_x)
 	min_d2,distances2=min_partial_distance(points2,half_x)
 	d=min(min_d1,min_d2)
 	points1=strip_points(points1,distances1,d)
 	points2=strip_points(points2,distances2,d)
 	points=points1+points2
+	points = sorted(points, key=lambda point: point[1])
 	min_final_d=min_final_distance(points)
 	return print(min_final_d)
 
