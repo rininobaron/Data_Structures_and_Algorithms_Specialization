feac5c011355502387dc5004d58329d2c68b30e4
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-11 03:15:48 -0600

    Use recursion in order to overcome time issue
    Failed case #13/23: time limit exceeded (Time used: 20.00/10.00, memory used: 24502272/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/7_closest_points/closest.py
+++ COURSE_1_Algorithmic_Toolbox/Week4/Assignment_4_Divide_and_Conquer/7_closest_points/closest.py
@@ -10,19 +10,6 @@ def min_partial_distance(points):
 		min_d = min(min_d, distance(point1, point2))
 	return min_d
 
-# def min_partial_distance(points):
-# 	min_d=float("inf")
-# 	counter=0
-# 	for point in points:
-# 		for index,point2 in enumerate(points):
-# 			if counter==index:
-# 				continue
-# 			d=distance(point,point2)
-# 			if d<min_d:
-# 				min_d=d
-# 		counter+=1
-# 	return min_d
-
 def strip_points(half_x,d):
 	def calculate_distance(point):
 		return abs(point[0]-half_x)<=d
@@ -43,18 +30,17 @@ def min_final_distance(points,d):
 			j+=1
 	return min_d
 
-def min_global_distance():
-	n=int(input(""))
-	points=[]
-	for _ in range(n):
-		x,y=map(int,input("").split(" "))
-		points.append((x,y))
-	points.sort()
+def min_global_distance(points):
+	if len(points)<=3:
+		return min_partial_distance(points)
+	n=len(points)
 	points1=points[:n//2]
 	points2=points[n//2:]
 	half_x=points[n//2][0]
-	min_d1=min_partial_distance(points1)
-	min_d2=min_partial_distance(points2)
+	#min_d1=min_partial_distance(points1)
+	#min_d2=min_partial_distance(points2)
+	min_d1=min_global_distance(points1)
+	min_d2=min_global_distance(points2)
 	d=min(min_d1,min_d2)
 	points1=list(filter(strip_points(half_x,d),points1))
 	points2=list(filter(strip_points(half_x,d),points2))
@@ -62,6 +48,12 @@ def min_global_distance():
 	points = sorted(points, key=lambda point: point[1])
 	d_prime=min_final_distance(points,d)
 	min_final_d=min(d,d_prime)
-	return print(min_final_d)
+	return min_final_d
 
-min_global_distance()
+n=int(input(""))
+points=[]
+for _ in range(n):
+	x,y=map(int,input("").split(" "))
+	points.append((x,y))
+points.sort()
+print(min_global_distance(points))
