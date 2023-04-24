82b248833724d772a86c6245b4560ee38d0aa81d
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-24 06:37:15 -0600

    Improve greedy algorithm

--- COURSE_1_Algorithmic_Toolbox/Week5/2_primitive_calculator/primitive_calculator.py
+++ COURSE_1_Algorithmic_Toolbox/Week5/2_primitive_calculator/primitive_calculator.py
@@ -1,19 +1,19 @@
 def calculator(n):
 	numCalculations=0
-	currentNumber=1
-	intermediateNumbers=[currentNumber]
-	while currentNumber < n:
-		if currentNumber<=n/3:
-			currentNumber=3*currentNumber
-			intermediateNumbers.append(currentNumber)
-		elif currentNumber<=n/2:
-			currentNumber=2*currentNumber
-			intermediateNumbers.append(currentNumber)
+	currentNumber=n
+	intermediateNumbers=[str(currentNumber)]
+	while currentNumber > 1:
+		if currentNumber%3==0:
+			currentNumber=currentNumber/3
+		elif currentNumber/10==1:
+			currentNumber-=1
+		elif currentNumber%2==0:
+			currentNumber=currentNumber/2
 		else:
-			currentNumber+=1
-			intermediateNumbers.append(currentNumber)
+			currentNumber-=1
+		intermediateNumbers.append(str(int(currentNumber)))
 		numCalculations+=1
-	intermediateNumbers=" ".join([str(i) for i in intermediateNumbers])
+	intermediateNumbers=" ".join(reversed(intermediateNumbers))
 	return print(str(numCalculations)+"\n"+intermediateNumbers)
 
 n = int(input())
