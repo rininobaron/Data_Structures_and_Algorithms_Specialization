6815d03ecb1ee8ec2427e0c88bb67b8ec23ab494
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-27 07:01:23 -0600

    Final Solution

--- COURSE_1_Algorithmic_Toolbox/Week5/2_primitive_calculator/primitive_calculator.py
+++ COURSE_1_Algorithmic_Toolbox/Week5/2_primitive_calculator/primitive_calculator.py
@@ -1,23 +1,51 @@
 def calculator(n):
-	numCalculations=0
-	currentNumber=n
-	intermediateNumbers=[str(currentNumber)]
-	while currentNumber > 1:
-		if currentNumber/10==1:
-			currentNumber-=1
-		elif currentNumber%2==0:
-			if currentNumber==4:
-				currentNumber-=1
-			else:
-				currentNumber=currentNumber/2
-		elif currentNumber%3==0:
-			currentNumber=currentNumber/3
-		else:
-			currentNumber-=1
-		intermediateNumbers.append(str(int(currentNumber)))
-		numCalculations+=1
-	intermediateNumbers=" ".join(reversed(intermediateNumbers))
-	return print(str(numCalculations)+"\n"+intermediateNumbers)
+	if n == 1:
+		return print("0\n1")
+	iter_dict={'0':[n]}
+	listIndexes=[[None]]
+	i=1
+	while 1 not in iter_dict[str(i-1)]:
+		iter_dict[str(i)]=[]
+		tempIndexes=[]
+		for j in range(len(iter_dict[str(i-1)])):
+			if iter_dict[str(i-1)][j]>1:
+				iter_dict[str(i)].append(iter_dict[str(i-1)][j] - 1)
+				tempIndexes.append(j)
+			if iter_dict[str(i-1)][j]%2==0:
+				iter_dict[str(i)].append(iter_dict[str(i-1)][j]/2)
+				tempIndexes.append(j)
+			if iter_dict[str(i-1)][j]%3==0:
+				iter_dict[str(i)].append(iter_dict[str(i-1)][j]/3)
+				tempIndexes.append(j)
+		listIndexes.append(tempIndexes)
+		i+=1
+	i-=1
+	intermediateNumbers=[]
+	index=iter_dict[str(i)].index(1)
+	next_index=listIndexes[i][index]
+	intermediateNumbers.append(1)
+	i-=1
+	while i>0:
+		intermediateNumbers.append(int(iter_dict[str(i)][next_index]))
+		next_index=listIndexes[i][next_index]
+		i-=1
+	intermediateNumbers.append(n)
+	#print(intermediateNumbers)
+	#print(len(intermediateNumbers)-1)
+	# print()
+	# print("len dict: "+str(len(iter_dict.keys())))
+	# print("len indexes: "+str(len(listIndexes)))
+	# print(i)
+	# for i in range(len(iter_dict.keys())):
+	# 	#print("Indexes")
+	# 	#print(i, len(listIndexes[i]))
+	# 	print(listIndexes[i])
+	# 	print("Numbers")
+	# 	print(i, len(iter_dict[str(i)]))
+	# 	print(iter_dict[str(i)])
+	counterNumers=len(intermediateNumbers)-1
+	intermediateNumbers=" ".join([str(i) for i in intermediateNumbers])
+	return print(str(counterNumers)+"\n"+intermediateNumbers)
 
 n = int(input())
 calculator(n)
