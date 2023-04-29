32c83190e454cba601ea883779551b3374e97b20
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-04-29 03:32:22 -0600

    Final solution
    Overcome the error below:
    Failed case #14/37: Wrong answer
     (Time used: 0.01/5.00, memory used: 11264000/2147483648.)

--- COURSE_1_Algorithmic_Toolbox/Week5/4_longest_common_subsequence_of_two_sequence/lcs2.py
+++ COURSE_1_Algorithmic_Toolbox/Week5/4_longest_common_subsequence_of_two_sequence/lcs2.py
@@ -1,41 +1,12 @@
-def builder_matrix_t(a, b):
-	T=[[float("inf")]*(len(b)+1) for _ in range(len(a)+1)]
-	for i in range(len(a)+1):
-		T[i][0]=i
-	for j in range(len(b)+1):
-		T[0][j]=j
+def lcsv2(a, b):
+	T=[[0]*(len(b)+1) for _ in range(len(a)+1)]
 	for i in range(1, len(a)+1):
 		for j in range(1, len(b)+1):
 			if a[i-1]==b[j-1]:
-				diff=0
+				T[i][j]=T[i-1][j-1]+1
 			else:
-				diff=1
-			T[i][j]=min(T[i][j-1]+1, T[i-1][j]+1, T[i-1][j-1]+diff)
-	return T
-
-def counter(T,a,b):
-	i=len(a)
-	j=len(b)
-	counter=0
-	while i > 0 or j > 0:
-		if T[i][j]==T[i-1][j]+1:
-			# Deletion
-			i-=1
-		elif T[i][j]==T[i][j-1]+1:
-			# Insertion
-			j-=1
-		else:
-			if a[i-1]==b[j-1]:
-				# Match
-				counter+=1
-			i-=1
-			j-=1
-	return counter
-
-def lcsv2(a,b):
-	T=builder_matrix_t(a, b)
-	counter=counter(T,a,b)
-	return print(counter)
+				T[i][j]=max(T[i][j-1], T[i-1][j], T[i-1][j-1])
+	return T[len(a)][len(b)]
 
 n = int(input())
 a = input().split()
@@ -49,4 +20,4 @@ b = [int(i) for i in b]
 if m != len(b):
 	print("Error!\nThe sequence doesn't have {} numbers".format(m))
 	exit()
-lcsv2(a, b)
+print(lcsv2(a, b))
