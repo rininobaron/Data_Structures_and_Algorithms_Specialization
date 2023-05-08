5c046a2772e3d584776af0d819b142e05af871e6
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-05-08 17:35:28 -0600

    Final solution

--- COURSE_1_Algorithmic_Toolbox/Week6/1_maximum_amount_of_gold/knapsack.py
+++ COURSE_1_Algorithmic_Toolbox/Week6/1_maximum_amount_of_gold/knapsack.py
@@ -1,11 +1,14 @@
 def max_amount(W,n,weights):
-	weights=sorted(weights)
-	weights.reverse()
-	remainW=W
-	for w in weights:
-		if w <= remainW:
-			remainW-=w
-	return W-remainW
+	value=[[0]*(n+1) for _ in range(W+1)]
+	v=weights.copy()
+	for i in range(1,n+1):
+		for w in range(1,W+1):
+			value[w][i]=value[w][i-1]
+			if weights[i-1]<=w:
+				val=value[w-weights[i-1]][i-1] + v[i-1]
+				if value[w][i] < val:
+					value[w][i]=val
+	return value[-1][-1]
 
 W, n = input().split()
 W, n = int(W), int(n)
