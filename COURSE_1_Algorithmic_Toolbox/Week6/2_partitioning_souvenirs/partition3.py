7153eed4f1477145140fa81018b852dc6ddf240d
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-05-14 09:19:38 -0600

    Final solution using promt engineering

--- COURSE_1_Algorithmic_Toolbox/Week6/2_partitioning_souvenirs/partition3.py
+++ COURSE_1_Algorithmic_Toolbox/Week6/2_partitioning_souvenirs/partition3.py
@@ -1,31 +1,28 @@
-def max_amount(W,n,weights):
-	value=[[0]*(n+1) for _ in range(W+1)]
-	v=weights.copy()
-	for i in range(1,n+1):
-		for w in range(1,W+1):
-			value[w][i]=value[w][i-1]
-			if weights[i-1]<=w:
-				val=value[w-weights[i-1]][i-1] + v[i-1]
-				if value[w][i] < val:
-					value[w][i]=val
-	return value
+def can_split_into_three_sets(numbers):
+    if len(numbers)<3:
+        return 0
+    total_sum = sum(numbers)
+    if total_sum % 3 != 0:
+        return 0
+    target_sum = total_sum // 3
+    set_sums = [0, 0, 0]
+    return backtrack(numbers, 0, set_sums, target_sum)
 
-def split_loot(n, numbers):
-	if len(numbers)<3:
-		return 0
-	if sum(numbers)%3!=0:
-		return 0
-	third_W=int(sum(numbers)/3)
-	value=max_amount(sum(numbers),len(numbers),numbers)
-	if value[third_W][-1]==third_W and value[2*third_W][-1]==2*third_W and value[3*third_W][-1]==3*third_W:
-		return 1
-	else:
-		return 0
+def backtrack(numbers, index, set_sums, target_sum):
+    if index == len(numbers):
+        return set_sums[0] == set_sums[1] == set_sums[2] == target_sum
+    for i in range(3):
+        if set_sums[i] + numbers[index] <= target_sum:
+            set_sums[i] += numbers[index]
+            if backtrack(numbers, index + 1, set_sums, target_sum):
+                return 1
+            set_sums[i] -= numbers[index]
+    return 0
 
 n=int(input())
 numbers=input().split()
 numbers=[int(i) for i in numbers]
 if n!=len(numbers):
-	print("ERROR!\nThe total numbers are not "+str(n))
-	exit()
-print(split_loot(n, numbers))
+    print("ERROR!\nThe total numbers are not "+str(n))
+    exit()
+print(can_split_into_three_sets(numbers))
