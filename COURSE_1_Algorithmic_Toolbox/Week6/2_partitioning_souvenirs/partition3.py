779b8f50fd15ac1647a795d5f0a4ef11a73dc374
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-05-13 21:23:31 -0600

    Change approach in order to overcome issue in case 24

--- COURSE_1_Algorithmic_Toolbox/Week6/2_partitioning_souvenirs/partition3.py
+++ COURSE_1_Algorithmic_Toolbox/Week6/2_partitioning_souvenirs/partition3.py
@@ -1,4 +1,4 @@
-def max_amount2(W,n,weights):
+def max_amount(W,n,weights):
 	value=[[0]*(n+1) for _ in range(W+1)]
 	v=weights.copy()
 	for i in range(1,n+1):
@@ -8,35 +8,19 @@ def max_amount2(W,n,weights):
 				val=value[w-weights[i-1]][i-1] + v[i-1]
 				if value[w][i] < val:
 					value[w][i]=val
-	final_list=[]
-	while i>=1 and w>=1:
-		val=value[w-weights[i-1]][i-1] + v[i-1]
-		val2=value[w][i-1]
-		if w-weights[i-1]<0:
-			i-=1
-			continue
-		if val > val2:
-			final_list.append(v[i-1])
-			w=w-weights[i-1]
-			i-=1
-		else:
-			i-=1
-	return value[-1][-1], list(reversed(final_list))
-
+	return value
 
 def split_loot(n, numbers):
 	if len(numbers)<3:
 		return 0
 	if sum(numbers)%3!=0:
 		return 0
-	target=int(sum(numbers)/3)
-	for i in range(3):
-		result, temp_list=max_amount2(target,len(numbers),numbers)
-		if result!=target:
-			return 0
-		for number in temp_list:
-			numbers.remove(number)
-	return 1
+	third_W=int(sum(numbers)/3)
+	value=max_amount(sum(numbers),len(numbers),numbers)
+	if value[third_W][-1]==third_W and value[2*third_W][-1]==2*third_W and value[3*third_W][-1]==3*third_W:
+		return 1
+	else:
+		return 0
 
 n=int(input())
 numbers=input().split()
